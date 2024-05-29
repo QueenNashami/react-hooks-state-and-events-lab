3e9e89d0739a9e3cafe8718d48ffe9cd0dbf8c0d
Brigid Wanza <wanzanashami@gmail.com>
2024-05-29 11:22:11 +0300

    Shows a well working shopping list

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  const handleAddToCart = () => {
+    setInCart(true);
+  };
+
+  const itemClass = inCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>
+        Add to Cart
+      </button>
     </li>
   );
 }
